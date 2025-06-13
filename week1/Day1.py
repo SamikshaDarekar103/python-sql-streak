7c6fff734fad69583a9189cdde0e6c725339d105
Samiksha Darekar <darekarsamiksha78@gmail.com>
2025-06-13 19:33:03 +0530

    Add files via upload

---
+++ week1/Day1.py
@@ -0,0 +1,16 @@
+#Day2:Python Basics: Write a script: Hello World, user input
+
+print("Hello World")#In python we use print() function to print output in screen
+print(30)
+print(30+35)#We can also perform operations like addition,substraction,multiplication and division in this function
+
+####Taking User Input in Python
+age =input("Enter your age")# To take user input "input()" function is used
+print("Your age is",age)
+print(type(age))##Here the data type of age in string even though it is number
+
+###All inputs are string By default
+### To use other data types, convert them
+marks=int(input("Enter your marks"))##Converting the input into integer
+print(marks)
+print(type(marks))###Here the datatype of marks in integer 
