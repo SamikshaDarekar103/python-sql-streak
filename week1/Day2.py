93b1af50dae0a5da3c88b84abc76617c9f70cf39
Samiksha Darekar <darekarsamiksha78@gmail.com>
2025-06-14 19:50:55 +0530

    Add files via upload

---
+++ week1/Day2.py
@@ -0,0 +1,22 @@
+# Day 2 - Data Types: Create variables: int, str, flot,boolean.
+##Python support various datatypes some of them are listed below
+marks=50 # Integer Datatype
+perentage=89.89  # Float Datatype
+boy="John" #String Datatype String can be declared within " " and ' ' and ''' '''
+decision=True #Boolean Datatype
+
+age = 25
+print("Age:", age)
+print("Type of age:", type(age))
+
+height = 5.9
+print("Height:", height)
+print("Type of height:", type(height))
+
+name ='''Sam'''
+print("Name:", name)
+print("Type of name:", type(name))
+
+is_student = True
+print("Is student:", is_student)
+print("Type of is_student:", type(is_student))
