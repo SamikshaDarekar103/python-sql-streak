d8f91a2e6c3d7de0fda69a38fad44cc40b0374b4
Samiksha Darekar <darekarsamiksha78@gmail.com>
2025-07-04 23:40:11 +0530

    Add files via upload

---
+++ week3/Day16.py
@@ -0,0 +1,19 @@
+#Day16:Write a Function for Fibonacci series 
+def fibonacci(n):
+    n1, n2 = 0, 1
+    count = 0
+    print("Fibonacci sequence:")
+    while count < n:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+
+# Get user input
+num_terms = int(input("How many terms? "))
+
+if num_terms <= 0:
+    print("Please enter a positive integer.")
+else:
+    fibonacci(num_terms)
