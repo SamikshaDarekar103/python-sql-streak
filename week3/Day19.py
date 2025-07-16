72747d34992a7c59e569c9461f45d0a976322a9f
Samiksha Darekar <darekarsamiksha78@gmail.com>
2025-07-16 23:32:42 +0530

    Update Day19.py

--- week3/Day19.py
+++ week3/Day19.py
@@ -44,3 +44,22 @@ WM1=WashingMachine("LG",220,10)
 WM1.show_capacity()
 WM1.show_specification()
 
+#Oparator Overloading:Operator overloading means giving special meaning to standard operators.when they are used with user-defined objects.
+#Adding two Comple numbers
+class complex:
+    def __init__(self,real,imag):
+        self.real=real
+        self.imag=imag
+    def showNumber(self):
+        print(self.real,"i+",self.imag,"j")
+    def __add__(self,num2):       #Dunder Function:A method with double underscores used for special behavior
+        newReal=self.real+num2.real
+        newImag=self.imag+num2.imag
+        return complex(newReal,newImag)
+
+num1=complex(1,2)
+num2=complex(3,4)
+num1.showNumber()
+num2.showNumber()
+num3=num1+num2
+num3.showNumber()
